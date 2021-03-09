f9dfcf4f263c883826338c27e9b918c17ef9b9ff
repl.it user <replituser@example.com>
2021-03-09 11:27:10 +0000

    gata

--- main.py
+++ main.py
@@ -1,56 +1,30 @@
-<<<<<<< HEAD
+# Define a function for calculate the area of triangle
+def calculate_area(h = 10,b = 5):
+ 
+  area = 0.5*h*b
+  return area
+
+def run():
+  x = calculate_area(4)
+  x += 1
+  print(f"The area of triangle is {x}")
+ 
+run()
+#print(calculate_area())
+#x = calculate_area(12,5)
+#print(x+1)
 
 
+#Call the function
+#calculate_area(h,b)
 
-print("Please choose from menu:\n\n1-Nice message\n2-Area of rectangle\n3-Area of triangle\n4-Times table")
-option = int(input())
-
-if option == 1:
-
-  print("Today will be a good day!")
-elif option == 2:
-  print("Enter the lenght of the rectangle")
-  l = int(input())
-  print("Enter the width of the rectangle")
-  w = int(input())
-  area = w*l
-  print("The area of the rectangle was {}".format(area))
-elif option == 3:
-  print("Enter the lenght of the triangle")
-  base = int(input())
-  print("Enter the width of the triangle")
-  height = int(input())
-  area = 0.5*base*height
-  print("The area of the triangle was {:.2f}".format(area))
-elif option == 4:
-  print("What number would you like to see times table for?")
-  n = int(input())
-  for i in range(1,11,1):
-    print("{}*{} = {}".format(n,i,n*i))
-else:
-  print("There is no such option, restart!")
-
-
-=======
-# while loop  (and also for loop) can be used as a repetition
-
-
-print("How many times you want to print the symbol?")
-x = int(input())
-
-i = 0
-# i is a counter. keeps track of how many times goes to the loop
-
-while i < x:
-  print("\u27bd", i)
-  i = i + 1
-print("We left the loop")
-
-
-
->>>>>>> d85530742e86480ec82d71c9ccac7c4748f38881
+#calculate_area()
+#calculate_area(5)
+#calculate_area(b=20)
+#calculate_area(13,7)
 
 
 
 
 
+#Parameter  - a valuw you plug into a function
